27f55022845d09a4a845c2478f04b06e2b277b26
omarayub22 <omar.ayub2@gmail.com>
2021-01-15 20:55:51 +0500

    first commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    }
+}
+
+
+// what is ORM(An Object Relational Mapper)
+// Translate our code into SQL commands that update our tables in the database
+// LING-dbContext.Users.Add(User{id=4,Name= "Omar"})
+// Entity Framework Features
+//-- Querying,Change tracking, Saving, concurancy, Transaction, Caching, Builtin convention,congiguration,Migrations
